500957e34e2533f7d2e2cfea67a6bdf4105b0c78
Clinton Morrison <contact@clintonmorrison.com>
2017-07-19 00:10:56 -0400

    some work on string index class

--- src/StringIndex.js
+++ src/StringIndex.js
@@ -5,6 +5,43 @@ class StringIndex {
   constructor() {
     this.suffixTree = new SuffixTree([]);
   }
+
+  whereStringEquals() {
+    const processedPattern = `^${this._encode(pattern)}$`;
+    return this.suffixTree.getMatchingStringIDs(processedPattern);
+  }
+
+  whereStringContains(pattern) {
+    const processedPattern = `^${this._encode(pattern)}`;
+    return this.suffixTree.getMatchingStringIDs(processedPattern);
+  }
+
+  whereStringEndsWith(pattern) {
+    const processedPattern = `^${this._encode(pattern)}`;
+    return this.suffixTree.getMatchingStringIDs(processedPattern);
+  }
+
+  whereStringStartsWith(pattern) {
+    const processedPattern = this._encode(pattern);
+    return this.suffixTree.getMatchingStringIDs(processedPattern);
+  }
+
+  add(string) {
+    this.suffixTree.add(`^${this._encode(pattern)}$`);
+  }
+
+  static _encode(string) {
+    return string
+      .replace(/\$/g, '\\$')
+      .replace(/\^/g, '\\^');
+  }
+
+  static _decode(string) {
+    return string
+      .replace(/\\\$/, '$')
+      .replace(/\\\^/, '^')
+      .slice(1, -1);
+  }
 }
 
 module.exports = StringIndex;
---
+++ test/StringIndex.test.js
@@ -0,0 +1,26 @@
+import StringIndex from '../src/StringIndex';
+
+
+describe('StringIndex#_encode', () => {
+  it('should replace "$" with "\\$"', () => {
+    StringIndex._encode('ABCD$ TEST$').should.be.exactly('ABCD\\$ TEST\\$');
+  });
+
+  it('should replace "^" with "\\^"', () => {
+    StringIndex._encode('ABCD^ TEST^').should.be.exactly('ABCD\\^ TEST\\^');
+  });
+});
+
+describe('StringIndex#_decode', () => {
+  it('should remove a leading "^" and trailing "$" characters', () => {
+    StringIndex._decode('^test$').should.be.exactly('test');
+  });
+
+  it('should replace "\\^" with "^"', () => {
+    StringIndex._decode('^test\\^test$').should.be.exactly('test^test');
+  });
+
+  it('should replace "\\$" with "$"', () => {
+    StringIndex._decode('^test\\$test$').should.be.exactly('test$test');
+  });
+});
